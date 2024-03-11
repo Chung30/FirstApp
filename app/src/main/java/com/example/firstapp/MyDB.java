ae21d2331e28bf4a5fdf901cb8330d5ccff4d6b7
Chung30 <lhongquangtrung@gmail.com>
2024-03-11 16:12:44 +0700

    # add common

---
+++ app/src/main/java/com/example/firstapp/MyDB.java
@@ -0,0 +1,30 @@
+package com.example.firstapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDB extends SQLiteOpenHelper {
+
+    public static final String TableName = "ContactTable";
+    public static final String Id = "Id";
+    public static final String Name = "FullName";
+    public static final String Phone = "Phonenumber";
+    public static final String Image = "Image";
+
+    public MyDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
